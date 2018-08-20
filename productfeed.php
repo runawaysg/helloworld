6e232d0a708cc2165391c0510b2ea86767ea4496
runawaysg <42550898+runawaysg@users.noreply.github.com>
2018-08-21 01:54:36 +0800

    Add files via upload

---
+++ productfeed.php
@@ -0,0 +1,17 @@
+<?php
+header("Content-type: text/xml");
+echo "<?xml version='1.0' encoding='UTF-8'?>";
+echo "<Products>";
+echo "<Product 1>";
+echo "<SKU>LM2052-FLASH-SALE</SKU>";
+echo "<Name>[FLASH SALE - Walk Safari] Working Ladies Mid Heel Pointed Toe Stylish Office Leather Shoes (LM2052)</Name>";
+echo "<Description>Upper Calf Leather / Lining Sheep Leather / Sock Sheep Leather</Description>";
+echo "<URL>http://www.everyonefootwear.com/product-p/lm2052-flash-sale.htm</URL>";
+echo "<Price>129.00</Price>";
+echo "<LargeImage>http://www.everyonefootwear.com/v/vspfiles/photos/LM2052-FLASH-SALE-2T.png</LargeImage>";
+echo "<Brand>Walk Safari</Brand>";
+echo "<Category>shoes</Category>";
+echo "<Condition="new"></Condition>";
+echo "</Product 1>";
+echo "</Products>";
+?>
